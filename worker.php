4b8f50f8be96e11848b2f2e03199627b1f67212b
Kushagra <kushagr.singh3@gmail.com>
2012-08-26 14:11:30 +0530

    many workers fixes the freeze issue

--- worker.php
+++ worker.php
@@ -2,31 +2,20 @@
 	
 	require "predis/autoload.php";
 	Predis\Autoloader::register();
-	
-	//create new workers and assign functions
-	$getResponse_worker = new GearmanWorker();
-	$getResponse_worker->addServer('127.0.0.1', 4730);
-	$getResponse_worker->addFunction("getResponse","get_response_fn");
-
-	$convert_worker = new GearmanWorker();
-	$convert_worker->addServer('127.0.0.1', 4730);
-	$convert_worker->addFunction("convertHTMLToJSON","convert_html_to_json_fn");
-
-
-	$background_worker = new GearmanWorker();
-	$background_worker->addServer('127.0.0.1', 4730);
-	$background_worker->addFunction("storeRedis","store_in_redis_fn");
+		
+	$worker = new GearmanWorker();
+	$worker->addServer('127.0.0.1', 4730);
+	$worker->addFunction("getResponse","get_response_fn");
+	$worker->addFunction("convertHTMLToJSON","convert_html_to_json_fn");
+	$worker->addFunction("storeRedis","store_in_redis_fn");
 	
 	while (1) {
 		print "Waiting for a job \n";
 
-		$ret_get = $getResponse_worker->work();
-		$ret_convert = $convert_worker->work();
-		$ret_background = $background_worker->work();
-		if (($getResponse_worker->returnCode() != GEARMAN_SUCCESS) || ($background_worker->returnCode() != GEARMAN_SUCCESS) || ($convert_worker->returnCode() != GEARMAN_SUCCESS))
+		$ret = $worker->work();
+		if ($worker->returnCode() != GEARMAN_SUCCESS)
 			break;
 	}
-
 
 	function get_response_fn($job){
 		echo "Generating response\n";
@@ -38,7 +27,6 @@
 		curl_close($ch);
 		return $result; 
 	}
-
 
 	function convert_html_to_json_fn($job){
 		echo "Received job: " . $job->handle() . "\n";
@@ -54,25 +42,22 @@
 		$redis_store_client->doBackground('storeRedis',$workload);
 	}
 
-
 	function store_in_redis_fn($job){
 		echo "Received Redis job: " . $job->handle() . "\n";
 		$workload = $job->workload();
 		$arguments = unserialize($workload);
 		$response = $arguments[0];
 		$url = $arguments[1];
-		
 		//was recieving an error for port 6379; change the port number as to your liking
 		$redis = new Predis\Client('tcp://127.0.0.1:4567');
 		$domain = get_domain_name($url);
 		$date = date("d/m/y");
 		echo "Storing in redis\n";
-		$redis->sadd("$domain:$url:$date",$response);
+		$redis->set("$domain:$url:$date",$response);
 		$redis->sadd("$domain:$url:donedates",$date);
 		$redis->sadd("$domain",$url);
 		echo "Store complete\n";
 	}
-
 
 	function get_domain_name($url){
 		$nowww = ereg_replace('www\.','',$url);
