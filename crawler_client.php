4d45b0a66a82d250f5f59eb4344fe2ffe69879f1
Kushagra <kushagr.singh3@gmail.com>
2012-08-26 14:14:45 +0530

    many workers fixes the freeze issue

--- crawler_client.php
+++ crawler_client.php
@@ -8,10 +8,7 @@
 	$redis = new Predis\Client('tcp://127.0.0.1:4567');
 
 	echo "Sending Job\n";
-
 	
-
-		
 	function crawl($gearman_client, $redis_client, $url){
 	
 		$response = $gearman_client->do("getResponse",$url);	
@@ -34,36 +31,13 @@
 			$redis_client->sadd("urlset",$href_attr);			
 		}
 		$new_starting_point = $redis_client->spop("urlset");
-		echo "***************$new_starting_point******************\n";
 		return $new_starting_point;
-
 	}
 
-	
-	$starting_point = "http://www.hackernews.com";
-
+	$starting_point = "http://www.reddit.com";
 	$next_starting_point = crawl($client, $redis, $starting_point);
-	echo "out of crawl\n";
-	
 	while (true){
-		echo "#############$next_starting_point###############\n";
 		$next_starting_point = crawl($client, $redis ,$next_starting_point);
 	}
-
-	
-
-	// function fetch_links($html_response){
-	// 	$dom = new DOMDocument();
-	// 	@$dom->loadHTML($html_response);
-	// 	$links = $dom->getElementsByTagName('a');
-	// 	foreach($links as $link){
-	// 		$href_attr = $link->getAttribute('href');
-	// 		$redis->sadd("urlset",$href_attr);			
-	// 	}
-	// }	
-
-	
-
-
 
 ?>
